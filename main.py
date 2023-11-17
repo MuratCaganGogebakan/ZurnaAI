06d3203f9c0b600640f55a497475680c9cf89f18
Murat Göğebakan <36950001+MuratCaganGogebakan@users.noreply.github.com>
2023-11-17 23:40:59 +0300

    feat: Connect to openai API

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
-from zurnai.audit_file import open_file, write_report
+from zurnai.audit_file import write_report
 
 load_dotenv()
 
-write_report("./result.txt",open_file("zurnai/audit_file.py"))
+write_report("./testCode.sol")
 
--- zurnai/audit_file.py
+++ zurnai/audit_file.py
@@ -1,17 +1,29 @@
-# Desc: Bele veziyyetin icine soxum
+from openai import OpenAI
+from dotenv import load_dotenv
 
-def open_file(file_path):
+load_dotenv()
+
+client = OpenAI()
+
+def read_file(file_path):
     try:
         with open(file_path, 'r') as file:
-            first_line = file.readline().strip()
-            print(first_line)
+            return file.read()
     except Exception as e:
         print(f"An error occurred while reading the file: {e}")
-    return first_line
-
-def write_report(file_path, report):
+        return None
+    
+def write_report(file_path):
+    report = client.chat.completions.create(
+  model="gpt-4",
+  messages=[
+    {"role": "system", "content": "Analyze and explain the vulnerabilities in this solidity code:"},
+    {"role": "user", "content": read_file(file_path)},
+  ]
+)
     try:
-        with open(file_path, 'w') as file:
-            file.write(report)
+        with open("./report.txt", 'w') as file:
+            file.write(str(report.choices[0].message.content))
     except Exception as e:
         print(f"An error occurred while writing the file: {e}")
+        return None
